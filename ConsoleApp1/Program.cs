f7943afa551fde29cc73b154c4e41cdb0b18ddd8
Denys <navrotskyidenys@gmail.com>
2022-08-31 00:13:58 +0300

    Add right decision

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,8 +17,10 @@ namespace ConsoleApp1
     {
         public static int ComparePowers(int[] a, int[] b)
         {
-            double aaa = Math.Pow(a[0], a[1]);
-            double bbb = Math.Pow(b[0], b[1]);
+            //double aaa = Math.Pow(a[0], a[1]);
+            //double bbb = Math.Pow(b[0], b[1]);
+            double aaa = a[1] * Math.Log(a[0]);
+            double bbb = b[1] * Math.Log(b[0]);
             if (aaa == bbb)
             {
                 return 0;
