3f0d53b9c46b2c9e7d24725ef459e4009392f127
Denys <navrotskyidenys@gmail.com>
2022-08-30 23:45:10 +0300

    Add simple resalt

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Kata
+    {
+        public static int ComparePowers(int[] a, int[] b)
+        {
+            double aaa = Math.Pow(a[0], a[1]);
+            double bbb = Math.Pow(b[0], b[1]);
+            if (aaa == bbb)
+            {
+                return 0;
+            }
+            if (aaa > bbb)
+            {
+                return -1;
+            }
+            if (aaa < bbb)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
